44c5dceeb39a1230fce2167aa74d2c6b58e838bf
NickTsalyn <nicktsalyn@gmail.com>
2023-06-12 17:12:14 -0400

    create main

---
+++ src/js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  loop: false,
+  spaceBetween: 19,
+  slidesPerView: 'auto',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
