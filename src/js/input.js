dde39c027b6534ada942653417f6f6cc36cfb897
NickTsalyn <nicktsalyn@gmail.com>
2023-06-30 14:53:11 -0400

    update script

--- src/js/input.js
+++ src/js/input.js
@@ -1,15 +1,19 @@
-function checkInput () {
-    const inputField = document.getElementById('inputField')
-    const inputValue = inputField.value
-    const errorText = document.getElementById('errorText')
-    const patternValid = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/
-        if(!patternValid.test(inputField)) {
-            errorText.textContent = "Please enter a valid email address"
-            errorText.classList.add('error');
-        }
-        else {
-            errorText.textContent = ' '
-            errorText.classList.remove('error')
-        }
-        return errorText
-};
+const inputField = document.getElementById('inputField')
+const errorText = document.getElementById('errorText')
+inputField.addEventListener('blur', checkInput)
+
+function checkInput (evt) {
+    const email = evt.currentTarget.value
+    const pattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/.test(email)
+    if(pattern) {
+        inputField.classList.add('valid')
+        inputField.classList.remove('error')
+        errorText.textContent = " "
+    }
+    else {
+        inputField.classList.remove('valid')
+        inputField.classList.add('error')
+        errorText.textContent = "Please enter a valid email address"
+    }
+}
+
